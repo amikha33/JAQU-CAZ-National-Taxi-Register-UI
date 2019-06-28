a9ac2915b84bae0821c0e9c6e4a0011b3da19a39
Sergii Chub <likeuwill@yahoo.com>
2019-06-28 17:45:09 +0200

    nulldb fix

---
+++ config/initializers/new_framework_defaults.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# https://blog.bigbinary.com/2016/02/15/rails-5-makes-belong-to-association-required-by-default.html
+Rails.application.config.active_record.belongs_to_required_by_default = true
