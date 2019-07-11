9441d686388c8da1ffa8d93c85edd0a7b5d5c6fe
Sergii Chub <likeuwill@yahoo.com>
2019-07-11 16:13:55 +0300

    [NTR-155] Add google analytics (#9)

---
+++ app/javascript/packs/google_analytics.js
@@ -0,0 +1,10 @@
+document.addEventListener('turbolinks:load', function(event) {
+  if (document.body.dataset['ga'])
+  {
+    if (typeof gtag === 'function') {
+      gtag('config', document.body.dataset.ga, {
+        'page_location': event.data.url
+      })
+    }
+  }
+})
