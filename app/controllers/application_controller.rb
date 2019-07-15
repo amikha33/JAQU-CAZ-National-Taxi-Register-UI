f311eb8dbac705a002b96e60a37262cc56f29fb9
Sergii Chub <likeuwill@yahoo.com>
2019-07-15 16:01:33 +0300

    [NTR-258] Remove logout error message (#7)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
   def handle_exception(exception)
     redirect_to(upload_index_path, alert: exception.response[:errors])
   end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(_resource_or_scope)
+    user_session_path
+  end
 end
