7d910f1af6563eca2a33b5fc1e14beb7230ea435
Damian Jurowicz <d.jurowicz@exlabs.pl>
2019-07-15 11:17:57 +0200

    [NTR-232] Show active page on menu (#1)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def is_current_path?(path)
+    if (request.path_info == path) ||
+       (request.path_info == root_path && path == upload_index_path)
+      'govuk-header__navigation-item--active'
+    end
+  end
 end
