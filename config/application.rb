e7907f7c2b3bdde29ffc944f18ad05cd87a1431c
cruddasj <james.cruddas@informed.com>
2019-07-29 06:58:32 +0100

    Add AWS deploy steps (#36)

--- config/application.rb
+++ config/application.rb
@@ -4,10 +4,11 @@ require_relative 'boot'
 
 # This is not loaded in rails/all but inside active_record so add it if
 # you want your models work as expected
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_view/railtie'
-require 'sprockets/railtie'
+require 'rails/all'
+# require 'active_record/railtie'
+# require 'action_controller/railtie'
+# require 'action_view/railtie'
+# require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/initializers/env_variables_checker.rb
+++ config/initializers/env_variables_checker.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-%w[AWS_REGION AWS_COGNITO_CLIENT_ID S3_AWS_ACCESS_KEY_ID S3_AWS_SECRET_ACCESS_KEY S3_AWS_REGION
-   S3_AWS_BUCKET TAXI_PHV_REGISTER_API_URL].each do |var|
-  Rails.logger.debug("#{var} environment variable not set") if Dotenv::Railtie.load[var].blank?
-end
+# %w[AWS_REGION AWS_COGNITO_CLIENT_ID S3_AWS_ACCESS_KEY_ID S3_AWS_SECRET_ACCESS_KEY S3_AWS_REGION
+#    S3_AWS_BUCKET TAXI_PHV_REGISTER_API_URL].each do |var|
+#   Rails.logger.debug("#{var} environment variable not set") if Dotenv::Railtie.load[var].blank?
+# end
