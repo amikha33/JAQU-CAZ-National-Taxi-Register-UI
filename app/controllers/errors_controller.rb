71e5bd2275b7cfc5e66f63ff345d1764c820174f
Sergii Chub <s.chub@exlabs.pl>
2019-08-23 10:41:47 +0300

    [NT] Set default html response for errors (#67)

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
+  before_action :set_html_response_format
+
   def not_found
     render(status: :not_found)
   end
 
   def internal_server_error
     render(status: :internal_server_error)
+  end
+
+  private
+
+  def set_html_response_format
+    request.format = :html
   end
 end
