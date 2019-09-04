c1f0392474ed537fb5946c5bc0211278f3e544c5
cruddasj <james.cruddas@informed.com>
2019-09-04 11:33:20 +0100

    Add build utils (#87)

---
+++ build_utils/check_build_number.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+check="false"
+
+echo "Remote url $build_id_url"
+echo "Checking build number on remote host"
+
+until [ $check = "true" ]
+do
+
+if curl -s "$build_id_url" | grep "$build_id";
+then
+    echo "Build id found"
+    exit
+else
+    sleep 30;
+    echo "Waiting for build number ${build_id} to be returned by remote host...";
+fi
+done
