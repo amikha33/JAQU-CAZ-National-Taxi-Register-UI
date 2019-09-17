fdb7d0ab1cc20c01169c1896fb6b0b0feb6ebb11
Sergii Chub <s.chub@exlabs.pl>
2019-09-17 10:13:13 +0300

    [NTR-464] Add api wrapper documentation (#101)

--- app/api_wrappers/base_api.rb
+++ app/api_wrappers/base_api.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
+##
+# This is an abstract class used as a base for all API wrapper classes.
+#
+# It includes {HTTParty gem}[https://github.com/jnunemaker/httparty]
 class BaseApi
   include HTTParty
 
   class << self
+    ##
+    # Performs a HTTP request and returns a hash with parsed body.
+    #
+    # ==== Attributes
+    #
+    # * +method+ - string or symbol, a valid HTTP request verb eg. +:get+
+    # * +path+ - string, a path of a request eg. +'/users'+
+    # * +options+ - hash, options like headers, the request body, query params
+    #
+    # ==== Exceptions
+    #
+    # Exception are raised based on HTTP status of the response.
+    # Other HTTP statuses than 400, 404, 422 and errors during response parsing are treated as 500.
+    #
     def request(method, path, options = {})
       response_object = public_send(method, path, options)
       validate_response(response_object.response)
@@ -11,6 +29,11 @@ class BaseApi
 
     private
 
+    ##
+    # Validates an API response and parses its body from JSON.
+    # Returns a parsed JSON object to hash or
+    # raises exception if status is above 400 or parsing fails.
+    #
     def validate_response(response_struct)
       status = response_struct.code.to_i
       parsed_body = parse_body(response_struct.body)
@@ -21,6 +44,10 @@ class BaseApi
       raise ApiException.new(status, status_message, parsed_body)
     end
 
+    ##
+    # Parses response body and returns a hash.
+    # +JSON::ParserError+ is treated as 500.
+    #
     def parse_body(body)
       JSON.parse(body.presence || '{}')
     rescue JSON::ParserError
--- app/api_wrappers/register_checker_api.rb
+++ app/api_wrappers/register_checker_api.rb
@@ -1,27 +1,95 @@
 # frozen_string_literal: true
 
+##
+# This class wraps calls being made to the NTR backend API.
+# The base URL for the calls is configured by +TAXI_PHV_REGISTER_API_URL+ environment variable.
+#
+# All calls have the correlation ID and JSON content type added to the header.
+#
+# All methods are on the class level, so there is no initializer method.
 class RegisterCheckerApi < BaseApi
-  base_uri ENV['TAXI_PHV_REGISTER_API_URL']
+  base_uri ENV['TAXI_PHV_REGISTER_API_URL'] + '/v1/scheme-management/register-csv-from-s3'
 
   class << self
+    ##
+    # Calls +/v1/scheme-management/register-csv-from-s3/jobs+ endpoint with +POST+ method
+    # and returns a job UUID
+    #
+    # ==== Attributes
+    #
+    # * +file_name+ - Csv file name, eg. 'CAZ-2020-01-08-AuthorityID-1'
+    # * +correlation_id+ - Correlation id, eg '98faf123-d201-48cb-8fd5-4b30c1f80918'
+    #
+    # ==== Example
+    #
+    #    RegisterCheckerApi.register_job(
+    #     'CAZ-2020-01-08-AuthorityID-1',
+    #     '98faf123-d201-48cb-8fd5-4b30c1f80918'
+    #    )
+    #
+    # ==== Result
+    #
+    # Returns a UUID, eg. '2ad47f86-8365-47ee-863b-dae6dbf69b3e'.
+    #
     def register_job(file_name, correlation_id)
       log_call("Registering job with file name: #{file_name}")
-      response = request(:post, "#{base_path}/jobs",
+      response = request(:post, '/jobs',
                          body: register_body(file_name),
                          headers: custom_headers(correlation_id))
       response['jobName']
     end
 
+    ##
+    # Calls +/v1/scheme-management/register-csv-from-s3/jobs/:job_uuid+ endpoint with +GET+ method
+    # and returns a job status.
+    #
+    # ==== Attributes
+    #
+    # * +job_uuid+ - Job UUID, eg '2ad47f86-8365-47ee-863b-dae6dbf69b3e'
+    # * +correlation_id+ - Correlation id, eg '98faf123-d201-48cb-8fd5-4b30c1f80918'
+    #
+    # ==== Example
+    #
+    #    RegisterCheckerApi.job_status(
+    #     '2ad47f86-8365-47ee-863b-dae6dbf69b3e',
+    #     '98faf123-d201-48cb-8fd5-4b30c1f80918'
+    #    )
+    #
+    # ==== Result
+    #
+    # Returns a job status, eg. 'SUCCESS'.
+    #
     def job_status(job_uuid, correlation_id)
       log_call("Getting job status with job uuid: #{job_uuid}")
-      response = request(:get, "#{base_path}/jobs/#{job_uuid}",
+      response = request(:get, "/jobs/#{job_uuid}",
                          headers: custom_headers(correlation_id))
       response['status']
     end
 
+    ##
+    # Calls +/v1/scheme-management/register-csv-from-s3/jobs/:job_uuid+ endpoint with +GET+ method
+    # and returns the error messages as an array.
+    #
+    # ==== Attributes
+    #
+    # * +job_uuid+ - Job UUID
+    # * +correlation_id+ - Correlation id
+    #
+    # ==== Example
+    #
+    #    RegisterCheckerApi.job_errors(
+    #     '2ad47f86-8365-47ee-863b-dae6dbf69b3e',
+    #     '98faf123-d201-48cb-8fd5-4b30c1f80918'
+    #    )
+    #
+    # ==== Result
+    #
+    # Returns a nil if job status is not 'FAILURE'
+    # Returns a array if job status is 'FAILURE', eg. '['error message one', error message two]'
+    #
     def job_errors(job_uuid, correlation_id)
       log_call("Getting job errors with job uuid: #{job_uuid}")
-      response = request(:get, "#{base_path}/jobs/#{job_uuid}",
+      response = request(:get, "/jobs/#{job_uuid}",
                          headers: custom_headers(correlation_id))
       return nil unless response['status'] == 'FAILURE'
 
@@ -30,6 +98,18 @@ class RegisterCheckerApi < BaseApi
 
     private
 
+    ##
+    # Returns a register body as a json.
+    #
+    # ==== Attributes
+    #
+    # * +file_name+ - Csv file name,  eg. 'CAZ-2020-01-08-AuthorityID-1'
+    #
+    # ==== Result
+    #
+    # Returns a json,
+    #   eg. "{\"filename\":\"CAZ-2020-01-08-AuthorityID-1\",\"s3Bucket\":\"test-update-server\"}".
+    #
     def register_body(file_name)
       {
         "filename": file_name,
@@ -37,6 +117,20 @@ class RegisterCheckerApi < BaseApi
       }.to_json
     end
 
+    ##
+    # Add the correlation ID and JSON content type to the header.
+    #
+    # ==== Attributes
+    #
+    # * +correlation_id+ - Correlation ID,  eg. '98faf123-d201-48cb-8fd5-4b30c1f80918'
+    #
+    # ==== Result
+    #
+    # Returns a json, eg. {
+    #   "Content-Type"=>"application/json",
+    #   "X-Correlation-ID"=>"d39337ca-d94b-4332-9219-12f5a14740f3"
+    # }
+    #
     def custom_headers(correlation_id)
       {
         'Content-Type' => 'application/json',
@@ -44,10 +138,13 @@ class RegisterCheckerApi < BaseApi
       }
     end
 
-    def base_path
-      '/v1/scheme-management/register-csv-from-s3'
-    end
-
+    ##
+    # Logs given message at +info+ level with a proper tag.
+    #
+    # ==== Attributes
+    #
+    # * +msg+ - string, log message
+    #
     def log_call(msg)
       Rails.logger.info "[RegisterCheckerApi] #{msg}"
     end
