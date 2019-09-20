663e72d90b1c9e8b46a4f39af66abcf2cdd721ee
Wojtek Mikusek <w.mikusek@exlabs.pl>
2019-09-20 15:49:00 +0200

    [NT] Fix Cognito param filter (#108)

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,3 +4,19 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += %i[password confirmation_code]
+
+# https://github.com/aws/aws-sdk-ruby/issues/2122
+module Aws
+  module Log
+    class ParamFilter
+      def filter_hash(values)
+        filtered = {}
+        values.each_pair do |key, value|
+          key = key.downcase.to_sym
+          filtered[key] = @filters.include?(key) ? '[FILTERED]' : filter(value)
+        end
+        filtered
+      end
+    end
+  end
+end
