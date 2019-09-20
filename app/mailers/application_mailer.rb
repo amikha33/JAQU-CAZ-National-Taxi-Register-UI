cd85433c64c9246639573edd1d6b7df33a38feab
Sergii Chub <s.chub@exlabs.pl>
2019-09-20 10:33:12 +0300

    Features/ntr 467/add docs to services (#107)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,6 +4,6 @@
 # Base class for mailers. Sets default from value and layout
 #
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch('SES_FROM_EMAIL', 'TaxiandPHVCentralised.Database@defra.gov.uk')
+  default from: Rails.configuration.x.service_email
   layout 'mailer'
 end
--- app/services/email_validator.rb
+++ app/services/email_validator.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
+##
+# This class is used to validate email address.
+#
 class EmailValidator < BaseService
+  # Attribute used internally to save values.
   attr_reader :email
 
+  ##
+  # Initializer method.
+  #
+  # ==== Attributes
+  #
+  # * +email+ - string, eg. 'example@email.com'
   def initialize(email:)
     @email = email
   end
 
+  # The caller method for the service. It invokes validating.
+  #
+  # Returns a string if validation failed.
   def call
     if invalid_format?
       I18n.t('email.errors.invalid_format')
@@ -17,10 +30,16 @@ class EmailValidator < BaseService
 
   private
 
+  # Checks if email address not longer than 45 symbols.
+  #
+  # Returns boolean.
   def email_too_long?
     email.length > 45
   end
 
+  # Checks if email address is compliant with the naming rules.
+  #
+  # Returns boolean.
   def invalid_format?
     (email =~ URI::MailTo::EMAIL_REGEXP).nil?
   end
