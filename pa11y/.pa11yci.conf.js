9eb088bffd4c44fbe4fa7e91567ac706c8aa285d
Edmond Chhung <edmond.chhung@informed.com>
2019-09-24 23:23:40 +0100

    added pa11y

---
+++ pa11y/.pa11yci.conf.js
@@ -0,0 +1,91 @@
+var config = {
+    defaults: {
+        standard: 'WCAG2AA',
+        // ignore issue with role=presentation on start button
+        ignore: ["WCAG2AA.Principle1.Guideline1_3.1_3_1.F92,ARIA4"],
+        timeout: 5000,
+        wait: 1500,
+        chromeLaunchConfig: {
+            args: [
+                "--no-sandbox"
+            ]
+        },
+        hideElements: [".govuk-header__logotype-crown", ".govuk-footer__licence-logo"]
+    },
+    urls: [
+        '${BASE_URL}',,
+        '${BASE_URL}/cookies',
+        {
+            "url": '${BASE_URL}/passwords/reset',
+            "actions": [
+                "click element #forgot-password",
+                "set field #user_username to edmond,chhung@informed.com",
+            ]
+        },
+        {
+            "url": '${BASE_URL}/passwords/confirm_reset',
+            "actions": [
+                "click element #forgot-password",
+                "set field #user_username to edmond,chhung@informed.com",
+                "click element input[type=submit]",
+            ]
+        },
+        {
+            "url": '${BASE_URL}/upload/data_rules',
+            "actions": [
+                "set field #user_username to edmond,chhung@informed.com",
+                "set field #user_password to Edc123.,",
+                "click element .govuk-button--start",
+                "wait for element #vrn to be visible",
+                "click element input[type=submit]",
+                "wait for element #file-upload-1 to be visible",
+                "click element #data-rules",
+            ]
+        },
+        {
+            "url": '${BASE_URL}',
+            "actions": [
+                "set field #user_username to edmond,chhung@informed.com",
+                "set field #user_password to Edc123.,",
+                "click element .govuk-button--start",
+                "wait for element #vrn to be visible",
+                "click element input[type=submit]",
+                "wait for element #file-upload-1 to be visible"
+            ]
+        },
+    ]
+};
+
+/**
+ * Simple method to replace nested URLs in a pa11y configuration definition
+ */
+function replacePa11yBaseUrls(urls, defaults) {
+
+    console.error('BASE_URL:', process.env.BASE_URL);
+
+    //Iterate existing urls object from configuration
+    for (var idx = 0; idx < urls.length; idx++) {
+        if (typeof urls[idx] === 'object') {
+            // If configuration object in URLs is a further nested object, replace inner url field value
+            var nestedObject = urls[idx]
+            nestedObject['url'] = nestedObject['url'].replace('${BASE_URL}', process.env.BASE_URL)
+            urls[idx] = nestedObject;
+        } else {
+            // Otherwise replace simple string (see pa11y configuration guidance)
+            urls[idx] = urls[idx].replace('${BASE_URL}', process.env.BASE_URL);
+        }
+    }
+
+    result = {
+        defaults: defaults,
+        urls: urls
+    }
+
+	console.log('\n')
+	console.log('Generated pa11y configuration:\n')
+	console.log(result)
+	
+    return result
+};
+
+module.exports = replacePa11yBaseUrls(config.urls, config.defaults);
