e9ad5beaf24fcac2beee5eef68037a270597d9b1
Sergii Chub <s.chub@exlabs.pl>
2019-11-27 17:11:24 +0200

    [NT] Rename privacy policy to privacy notice (#201)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,8 +15,8 @@ describe StaticPagesController, type: :request do
     it_behaves_like 'a static page'
   end
 
-  describe 'GET #privacy_policy' do
-    subject(:http_request) { get privacy_policy_path }
+  describe 'GET #privacy_notice' do
+    subject(:http_request) { get privacy_notice_path }
 
     it_behaves_like 'a static page'
   end
