fead9a8f622748cd4035f7c900679e2b9841919f
cruddasj <james.cruddas@informed.com>
2021-08-19 15:57:20 +0100

    [CAZB-5333] Prevent build hangs with ECS rollout (#651)
    Prevent build hangs with ECS rollout by:
    
    1. Checking for presence of a build URL and if not set return a non-zero exit code
    1. Checking for presence of a build ID and if not set return a non-zero exit code
    1. Attempting at most 60 requests, which with a wait interval of 30 seconds equates to 30 minutes
    
    Co-authored-by: James Proctor <43146680+proctorj@users.noreply.github.com>

--- ci-cd-resources/check_build_number.sh
+++ ci-cd-resources/check_build_number.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 check="false"
+attempts=0
+max_attempts=60
+
+# Note that with the max_attempts being set at 60 if the build is not
+# found in 30 minutes, a non-zero exit code will be returned failing the build
+
+if [ -z "$build_id_url" ];
+then
+    echo "Error: Build ID URL variable not set"
+    exit 1
+fi
+
+if [ -z "$build_id" ];
+then
+    echo "Error: Build ID variable not set"
+    exit 1
+fi
 
 echo "Remote url $build_id_url"
-echo "Checking build number on remote host"
+echo "Checking for build id ${build_id} on remote host"
 
 until [ $check = "true" ]
 do
 
-if curl -s "$build_id_url" | grep -w "$build_id";
-then
-    echo "Build id found"
-    exit
-else
-    sleep 30;
-    echo "Waiting for build number ${build_id} to be returned by remote host...";
-fi
+  if [ "$attempts" -eq "$max_attempts" ];
+  then
+      echo "Build id not found after ${max_attempts} attempts"
+      exit 1
+  fi
+
+  if curl -s "$build_id_url" | grep -w "$build_id";
+  then
+      echo "Build id found"
+      exit
+  else
+      sleep 30;
+      attempts=$(( $attempts + 1 ))
+      echo "Waiting for build number ${build_id} to be returned by remote host...";
+  fi
+
 done
