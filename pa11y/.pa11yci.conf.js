e571859f7d796f51b8efefb0c55efc02f0e9d69b
Edmond Chhung <52657331+chhunge@users.noreply.github.com>
2021-09-06 14:31:25 +0100

    [CAZB-4100] Add pa11y tests for search page (#655)

--- pa11y/.pa11yci.conf.js
+++ pa11y/.pa11yci.conf.js
@@ -1,10 +1,13 @@
 var config = {
     defaults: {
-        standard: "WCAG2AA",
+        standard: 'WCAG2AA',
+        concurrency: 1,
         // ignore issue with role=presentation on start button
         ignore: ["WCAG2AA.Principle1.Guideline1_3.1_3_1.F92,ARIA4"],
-        timeout: 5000,
-        wait: 1500,
+        // The time in milliseconds that a test should be allowed to run before calling back with a timeout error.
+        timeout: 30000, // Maximum 30000ms(30 seconds)
+        // The time in milliseconds to wait before running HTML CodeSniffer on the page.
+        wait: 2500,
         chromeLaunchConfig: {
             args: [
                 "--no-sandbox"
@@ -13,54 +16,52 @@ var config = {
     },
     urls: [
         {
-            "url": "${BASE_URL}/cookie_control",
-            "actions": [
-                "wait for element #ccc-dismiss-button to be visible",
-                "click element #ccc-dismiss-button"
-            ]
-        },
-        {
-            "url": "${BASE_URL}/users/sign_in",
+            "url": "${BASE_URL}?home",
             "actions": [
                 "wait for element #ccc-dismiss-button to be visible",
                 "click element #ccc-dismiss-button",
+                ]
+        },
+        {
+            "url": "${BASE_URL}?upload",
+            "actions": [
                 "wait for element #user_username to be visible",
-                "wait for element #user_password to be visible",
                 "set field #user_username to tester@informed.com",
                 "set field #user_password to Tester123...",
                 "wait for element #sign_in_button to be visible",
                 "click element #sign_in_button",
-                "wait for element #file-upload-1 to be visible"
+                "wait for element #file-upload-1 to be visible",
             ]
         },
+        "${BASE_URL}/vehicles/search",
         {
-            "url": '${BASE_URL}?upload_page',
+            "url": "${BASE_URL}/vehicles/search?search-results",
             "actions": [
-                "wait for element #file-upload-1 to be visible"
-            ]
-        },
-        {
-            "url": '${BASE_URL}?data_rules',
-            "actions": [
-                "click element #data-rules",
-                "wait for path to be /upload/data_rules"
-            ]
-        },
-        {
-            "url": '${BASE_URL}?search',
-            "actions": [
-                "click element #search",
-                "wait for path to be /vehicles/search",
+                "wait for element #search_button to be visible",
                 "set field #vrn to IS19PCA",
-                "field #historical_off",
+                "click element #historical_off",
                 "click element #search_button",
-                "wait for element #new_search_button to be visible"
+                "wait for element #new_search_button to be visible",
             ]
         },
-        '${BASE_URL}/cookies',
-        '${BASE_URL}/accessibility_statement',
-        '${BASE_URL}/privacy_notice',
-        '${BASE_URL}/upload/success'
+        {
+            "url": "${BASE_URL}/vehicles/search?search-results-historical",
+            "actions": [
+                "wait for element #search_button to be visible",
+                "set field #vrn to IS19PCA",
+                "click element #historical_on",
+                "wait for element #search_start_date_day to be visible",
+                "set field #search_start_date_day to 01",
+                "set field #search_start_date_month to 01",
+                "set field #search_start_date_year to 2021",
+                "set field #search_end_date_day to 01",
+                "set field #search_end_date_month to 02",
+                "set field #search_end_date_year to 2021",
+                "click element #search_button",
+                "wait for element #search_another_vehicle to be visible",
+            ]
+        },
+
     ]
 };
 
